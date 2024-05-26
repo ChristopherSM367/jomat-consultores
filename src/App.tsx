fc02e11387b0d22acc199aaa141e9b6f1a50dd98
Christopher Sanchez <christopher.sanchez.mex367@gmail.com>
2024-05-25 21:37:24 -0600

    cambio link inicio

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/inicio" element={ <Home></Home> } />
+          <Route path="/" element={ <Home></Home> } />
           <Route path="/acerca-de" element={ <AboutUs></AboutUs> } />
           <Route path="/servicios" element={ <Services></Services> } />
           <Route path="/portafolio" element={ <Portfolio></Portfolio> } />
--- src/components/includes/Navbar.tsx
+++ src/components/includes/Navbar.tsx
@@ -6,18 +6,18 @@ export const Navbar = () => {
       <div className="container fnt_title">
         <div className="row align-items-center justify-content-between d-flex">
           <div id="logo">
-            <a href="inicio" title="inicio"><img src="/assets/img/logo/logo-bco.png" id="logo-img" alt="logo" /></a>
+            <a href="/" title="inicio"><img src="/assets/img/logo/logo-bco.png" id="logo-img" alt="logo" /></a>
           </div>
           <nav id="nav-menu-container">
             <ul className="nav-menu d-none d-lg-block sf-js-enabled sf-arrows">
-              <li className="nav-item dropdown d-none d-lg-block menualink"><a className="sf-with-ul" href="inicio">INICIO</a></li>
+              <li className="nav-item dropdown d-none d-lg-block menualink"><a className="sf-with-ul" href="/">INICIO</a></li>
               <li className="nav-item d-none d-lg-block menualink"><a className="sf-with-ul" href="acerca-de">NOSOTROS</a></li>
               <li className="nav-item d-none d-lg-block menualink"><a className="sf-with-ul" href="servicios">SERVICIOS</a></li>
               <li className="nav-item d-none d-lg-block menualink"><a className="sf-with-ul" href="portafolio">PORTAFOLIO</a></li>
               <li className="nav-item dropdown d-none d-lg-block menualink"><a className="sf-with-ul" href="contacto">CONTACTO</a></li>
             </ul>
             <ul className="navbar-nav mr-auto d-lg-none fnt_title">
-              <li><a href="inicio">INICIO</a></li>
+              <li><a href="/">INICIO</a></li>
               <li><a href="acerca-de">NOSOTROS</a></li>
               <li><a href="servicios">SERVICIOS</a></li>
               <li><a href="portafolio">PORTAFOLIO</a></li>
